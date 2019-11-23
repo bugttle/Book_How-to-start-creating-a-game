280310fba859dd0e41c51aa84db7dbfe11465855
bugttle <bugttle@gmail.com>
2019-11-23 13:14:31 +0900

    9.1 ブロックを並べる

---
+++ Chapter8-11_Plattach/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SceneControl : MonoBehaviour
+{
+    private BlockRoot block_root = null;
+
+    void Start()
+    {
+        // BlockRootスクリプトを取得
+        this.block_root = this.gameObject.GetComponent<BlockRoot>();
+        // BlockRootスクリプトのinitialSetUp()を呼び出す
+        this.block_root.initialSetUp();
+    }
+
+    void Update()
+    {
+    }
+}
